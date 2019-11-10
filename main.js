160ff89b971df3dad198f41fc1741e366bfaa361
乌鸦 <38463005+LiuYun18571620206@users.noreply.github.com>
2019-11-10 14:40:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -156,36 +156,59 @@ function createStore(reducer){
 }
 
 //原生call、apply、bind实现
-function call(context,arry){
+Function.prototype.myCall=function(context,...c){
+    if(typeof this!=='function'){
+        throw TypeError("调用该函数的上下文对象不是函数")
+    }
+    if(!this.prototype){
+        //判断是否为箭头函数
+        return this()
+    }
     context=context||window
-    arry=arry||[]
+    arry=c||[]
     let fn=this,sym=Symbol('fn')
-    context[sym]=fn
-    let value=context[sym](arry)
-    delete context[sym]
-    return value
-}
-function apply(context,...c){
-    context=context||window
-    let fn=this,sym=Symbol('fn')
-    context[sym]=fn
-    let value=context[sym](...c)
-    delete context[sym]
-    return value
-}
-function bind(context,...c){
-    context=context||window
-    let fn=this,sym=Symbol('fn')
-    return function(){
+    try{
         context[sym]=fn
-        let value=context[sym](...c)
-        delete context[sym]
-        return value
+        return context[sym](...arry)
+    }finally{
+        delete context
     }
 }
-Function.prototype.callA=call
-Function.prototype.applyA=apply
-Function.prototype.bindA=bind
+
+Function.prototype.myApply=function(context,arry){
+    if(typeof this!=='function'){
+        throw TypeError("调用该函数的上下文对象不是函数")
+    }
+    if(!this.prototype){
+        //判断是否为箭头函数
+        return this()
+    }
+    context=context||window
+    Array.isArray(arry)?void 0:arry=[]
+    let fn=this,sym=Symbol('fn')
+    try{
+        context[sym]=fn
+        return context[sym](...arry)
+    }finally{
+        delete context[sym]
+    }
+}
+
+Function.prototype.myBind=function(context,...c){
+    if(typeof this!=='function'){
+        throw TypeError("调用该函数的上下文对象不是函数")
+    }
+    context=context||window
+    let fn=this
+    let nop=function(...s){
+        if(Reflect.getPrototypeOf(this)===nop.prototype){
+            this.prototype=fn.prototype
+        }
+        console.log(context,c,fn)
+        return fn.myCall(context,...c,...s)
+    }
+    return nop
+}
 
 //Cookie操作
 function setCookie(name,value,time){
